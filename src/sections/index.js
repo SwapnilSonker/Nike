63c9789a739bff0ff1693129201d2e0738f1bda4
SwapnilSonker <swapnilsonker1432@gmail.com>
2023-12-30 11:47:07 +0530

    Shoecard section done

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import SuperQuality from "./SuperQuality";
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export  {
+  SuperQuality,
+  Hero,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  Footer,
+};
