a44679eeb3522a37ee73b79f9d0c3b1ec692cbab
SwapnilSonker <swapnilsonker1432@gmail.com>
2024-01-02 00:12:22 +0530

    deployment error fixation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,10 +13,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    // 'react-refresh/only-export-components': [
-    //   'warn',
-    //   { allowConstantExport: true },
-    // ],
-    // "react/prop-types": 0
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "react/prop-types": 0
   },
 }
